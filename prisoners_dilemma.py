b0da3eebcdbc2c6b44e3f3ab505c7c650f7c396a
16tsears <16tsears@student.nhart.org>
2014-11-17 13:39:49 -0500

    Sears group code
    The code made by the Sears group

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -543,19 +543,18 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
 
 
 
-    ######
-    ######
-    #
     elif player == 18:
-        if getting_team_name:
-            return 'loyal vengeful'
+        x = random.randint(1,2)
+    if getting_team_name:
+        return 'NICK IS AWESOME'
+    else:
+        if x == 1:
+            return 'c'
+        elif x == 2:
+            return 'b'
         else:
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
+            return 'b' #otherwise betray
+
     
     
 
